0e05f4bf4a3812cca6c8407a9a554104465e46de
LimeByte <limebyte@twf.co.nz>
2013-12-28 01:52:40 +1300

    List available teams when no team is specified.

--- src/com/eyeofender/dodgeball/command/TeamCommand.java
+++ src/com/eyeofender/dodgeball/command/TeamCommand.java
@@ -21,8 +21,20 @@ public class TeamCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (!(sender instanceof Player)) return false;
 
+        if (plugin.getGame().getArena() == null) return true;
+
         if (args.length < 1) {
+            StringBuilder builder = new StringBuilder();
+            for (DodgeTeam team : plugin.getGame().getArena().getTeams()) {
+                builder.append(team.getChatColour() + team.toString());
+                builder.append(ChatColor.GRAY + ", ");
+            }
+            builder.delete(0, builder.length() - 2);
+            builder.append(".");
+
             sender.sendMessage(ChatColor.RED + "Please specify a team.");
+            sender.sendMessage(ChatColor.AQUA + "Valid teams for this arena are:");
+            sender.sendMessage(builder.toString());
             return false;
         }
 
